621fe944f30c833a3ebaa4c4ebcb2871bd59b28c
Prateek Bhardwaj <54992024+prateekb1912@users.noreply.github.com>
2023-04-11 16:17:45 +0000

    Create base user profile model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    username = models.CharField(max_length=32)
+    followers = models.ManyToManyField(to=User, related_name='followers')
+    followings = models.ManyToManyField(to=User, related_name='followings')
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.username
