6c800703b47c439bc1a05a54e54e73502cb879d7
Prateek Bhardwaj <54992024+prateekb1912@users.noreply.github.com>
2023-04-11 16:49:08 +0000

    Add api route to authenticate user and return JWT

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import AuthenticateView
+
+urlpatterns = [
+    path('authenticate', AuthenticateView.as_view(), name='authenticate'),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import jwt
+from datetime import datetime, timedelta
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.conf import settings
 
-# Create your views here.
+class AuthenticateView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is None:
+            return Response({'error': 'Invalid credentials'}, status=400)
+
+        # Create JWT access token for 60 minutes 
+        payload = {
+            'id': user.id,
+            'expiry': datetime.utcnow() + timedelta(minutes=60),
+            'created_at': datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+        return Response({
+            'token': token
+        })
